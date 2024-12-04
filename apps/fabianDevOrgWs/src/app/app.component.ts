5806cc6785411b1d0f52e45d143d5b4fe72b18be
FabianRG1990 <fabian.soy1990@gmail.com>
2024-12-04 00:24:24 -0600

    feat: h1 hola mundo con componente
    aqui realize un h1 con el contenido de hola mundo. borre en contenido creado por nx para la pagina principal y modifique tanto el spec como el e2e que ambos estaban esperando un h1 con el contenido de welcome

--- apps/fabianDevOrgWs/src/app/app.component.ts
+++ apps/fabianDevOrgWs/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { SharedComponent } from 'libs/frontend/shared/src/lib/shared.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ RouterModule, SharedComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ libs/frontend/shared/src/lib/shared.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared',
+  standalone: true,
+  imports: [],
+  template: `
+  <h1>Hola Mundo</h1>
+  `,
+  styles: ``
+})
+export class SharedComponent {
+
+}
