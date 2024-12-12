33621a70dbba57617ee73e895797768d2eed3521
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2024-12-11 23:40:49 -0600

    feat: prefix
    correccion de el prefix para hacer que los selecttores de los componetes de dashboard sean fabian-dev en ves de lib-

--- libs/frontend/dashboard/eslint.config.js
+++ libs/frontend/dashboard/eslint.config.js
@@ -20,7 +20,7 @@ module.exports = [
         'error',
         {
           type: 'element',
-          prefix: 'lib',
+          prefix: 'fabian-dev',
           style: 'kebab-case',
         },
       ],
--- libs/frontend/dashboard/src/lib/dashboard/dashboard.component.ts
+++ libs/frontend/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-dashboard',
+  selector: 'fabian-dev',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './dashboard.component.html',
